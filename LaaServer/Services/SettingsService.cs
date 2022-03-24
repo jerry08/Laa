c64397db68dd39ad2d024e98d01293e9ae330795
Jeremy <74879119+jerry08@users.noreply.github.com>
2022-03-24 13:25:56 -0400

    Added size and location settings for main window

--- LaaServer/Services/SettingsService.cs
+++ LaaServer/Services/SettingsService.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Windows;
 using Tyrrrz.Settings;
 
 namespace LaaServer.Services
 {
     public class SettingsService : SettingsManager
     {
+        public double MainWindowHeight { get; set; }
+
+        public double MainWindowWidth { get; set; }
+
+        public double MainWindowTop { get; set; }
+
+        public double MainWindowLeft { get; set; }
+
+        public WindowState MainWindowState { get; set; }
+
         public bool IsAutoUpdateEnabled { get; set; } = true;
 
         public bool IsDarkModeEnabled { get; set; }
